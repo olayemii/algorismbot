99b7b00b03fec578587f059423c9140dea9c51f0
OLayemii Garuba <olayemi.garuba@algorismng.com>
2019-08-19 09:34:52 +0100

    fixes

--- worker.py
+++ worker.py
@@ -14,7 +14,6 @@ def worker():
         date_today = datetime.now().timetuple()
 
         print(f"{date_today.tm_hour, date_today.tm_min} Making a new check cause it's a 15 mins interval")
-        bot_obj.test_mention()
 
         # Check for Friday show and tell
         if (date_today.tm_wday, date_today.tm_hour, date_today.tm_min) == (4, 16, 0):
